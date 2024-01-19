a27ce1c3c2c98aa86ac9738e31817ef748f52a67
lup1n <780966523@qq.com>
2024-01-19 10:42:07 +0100

    0.0.26, user/password no more supported by Pypi, changed to token

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="motti",
-    version="0.0.25",
+    version="0.0.26",
     author="lup1n",
     author_email="780966523@qq.com",
     description="some util functions",
