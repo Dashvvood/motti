a3e1434ff753c14eee31b09f8bb0e9e01932803e
lup1n <780966523@qq.com>
2024-12-10 03:05:01 +0100

    :art:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="motti",
-    version="0.0.38",
+    version="0.0.39",
     author="lup1n",
     author_email="780966523@qq.com",
     description="some util functions",
